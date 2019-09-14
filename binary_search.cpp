c20084998b64df7f60dcbf5642f72d85bccc06f2
Camilo Fernández <camilofernandez@Camilos-iMac.local>
2019-09-13 20:12:05 -0500

    Función de lectura de archivo

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
-
+#include <stdlib.h>
+#include <string.h>
+#include <fstream>
 using namespace std;
+void lectura();
+
+void lectura(){
+    ifstream archivo;
+    string texto;
+
+    archivo.open("datos.csv",ios::in)
+
+    if(archivo.fail()){
+        cout<<"No se pudo abrir el archivo";
+        exit(1);
+    }
+
+    while(!archivo.eof()){
+        getline(archivo,texto);
+        cout<<texto<<endl;
+    }
+
+    archivo.close();
+
+}
+
+
+
+
+
+
+
+
+
 
 int main(){
 
 
 
-
+    system("pause");
     return 0;
 }
