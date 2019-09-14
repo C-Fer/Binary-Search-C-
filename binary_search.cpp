56f61258b76c027095cda0a977d441a6cac48b5a
Camilo Fernández <camilofernandez@Camilos-iMac.local>
2019-09-13 20:17:37 -0500

    modifiqué .csv a .txt

--- binary_search.cpp
+++ binary_search.cpp
@@ -9,7 +9,7 @@ void lectura(){
     ifstream archivo;
     string texto;
 
-    archivo.open("datos.csv",ios::in);
+    archivo.open("datos.txt",ios::in);
 
     if(archivo.fail()){
         cout<<"No se pudo abrir el archivo";
@@ -36,8 +36,6 @@ void lectura(){
 
 int main(){
     lectura();
-
-
 
     system("pause");
     return 0;
