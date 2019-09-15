a0d3bdac5ae0f88861f25579908c01b7f6a4b02f
Andres Restrepo <arestre1@msudenver.edu>
2019-09-14 20:20:51 -0600

    Added methods to pruebas.

--- pruebas.cpp
+++ pruebas.cpp
@@ -5,18 +5,65 @@
 
 using namespace std;
 
-int main()
+class Persona
+{    
+    public: 
+  
+    string nombre, cedula; 
+    
+    asignarNombre(string linea) 
+    { 
+        string delimitador = ";";
+        cedula = linea.substr(0, linea.find(delimitador));
+        nombre = linea.substr(linea.find(delimitador)+1, linea.length());    
+    } 
+
+    string nombreCedula(){
+        return nombre + " " + cedula;
+    }
+};
+
+class Registro
 {
+    public:
+
+    Persona personas[950];
+
+    void leerDatos()
+    {
 
         ifstream archivo_entrada("datos.csv");;
         string linea;
 
+        int index = 0;
+        while(getline(archivo_entrada, linea)) {       
+            Persona persona;
+            persona.asignarNombre(linea);
+            personas[index] = persona;
+            index = index +1; 
+        }     
+    }
 
-        while(getline(archivo_entrada, linea)) {
-
-            cout << linea[0] << endl;
-                
+    void imprimirLista()
+    {
+        for(int i =0 ; i <= sizeof(personas); i++){
+            cout << personas[i].nombreCedula()<< endl;
         }
+    }
+};
 
-        return EXIT_SUCCESS;
+
+class BusquedaBinary
+{
+
+};
+  
+
+int main()
+{
+    Registro regx;
+    regx.leerDatos();
+    regx.imprimirLista();
 }
+ 
+    
